c8ed3ef3d0d1dcaa85a1574e1c4e6cf787713c84
M Aiman Ismail <muhammadaimanbin.ismail@haw-hamburg.de>
2020-02-11 11:20:26 +0100

    handle floor passenger waiting

--- floor/floor.py
+++ floor/floor.py
@@ -4,14 +4,19 @@ import os
 import logging
 import argparse
 
+import json
 import paho.mqtt.client as mqtt
+
+from typing import List
+from cps_common.data import Passenger
 
 
 class Floor:
     def __init__(self, id: int):
-        self.id = id
+        self.floor: int = id
+        self.client = mqtt.Client(f"floor{self.floor}")
 
-        self.client = mqtt.Client(f"floor{self.id}")
+        self.waiting_list: List[Passenger] = []
 
     def run(self, host: str = "localhost", port: int = 1883):
         # setup MQTT
@@ -26,11 +31,11 @@ class Floor:
 
         subscriptions = [
             (
-                f"simulation/floor/{self.id}/passenger_waiting",
+                f"simulation/floor/{self.floor}/passenger_waiting",
                 self.on_passenger_waiting,
             ),
             (
-                f"simulation/floor/{self.id}/passenger_arrived",
+                f"simulation/floor/{self.floor}/passenger_arrived",
                 self.on_passenger_arrived,
             ),
         ]
@@ -48,8 +53,39 @@ class Floor:
     def on_passenger_waiting(self, client, userdata, msg):
         logging.info(f"New message from {msg.topic}")
 
+        # TODO: validate schema
+
+        # convert the payload to JSON
+        waiting_list = json.loads(msg.payload)
+        # convert the JSON to Passenger objects
+        self.waiting_list += [
+            Passenger(start_floor=s["start_floor"], end_floor=s["end_floor"])
+            for s in waiting_list
+        ]
+
+        self.push_call_button()
+
     def on_passenger_arrived(self, client, userdata, msg):
         logging.info(f"New message from {msg.topic}")
+
+    def push_call_button(self):
+        logging.info("pushing call button")
+
+        up: bool = False
+        down: bool = False
+        for p in self.waiting_list:
+            logging.debug(type(p.end_floor))
+            logging.debug(type(p))
+            logging.debug(type(up))
+            logging.debug(type(self.floor))
+            up = up or (p.end_floor > self.floor)
+            down = down or (p.end_floor < self.floor)
+        logging.debug(f"button pushed: up: {up}; down: {down}")
+
+        if up:
+            self.client.publish(f"floor/{self.floor}/button_pressed", "up")
+        if down:
+            self.client.publish(f"floor/{self.floor}/button_pressed", "down")
 
 
 if __name__ == "__main__":
@@ -85,9 +121,9 @@ if __name__ == "__main__":
 
     logging.basicConfig(level=getattr(logging, loglevel.upper()))
 
-    logging.info(f"Starting elevator {id}")
+    logging.info(f"Starting floor {id}")
 
-    controller = Floor(id=id)
+    controller = Floor(id=int(id))
     controller.run(host=host, port=int(port))
 
     logging.info(f"Exited elevator {id}")
