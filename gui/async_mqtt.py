b9f5dfbabf7f4ecf6cb73d5a8ec4afac23538b91
M Aiman Ismail <muhammadaimanbin.ismail@haw-hamburg.de>
2020-02-11 05:36:07 +0100

    handle waiting count

--- gui/async_mqtt.py
+++ gui/async_mqtt.py
@@ -6,7 +6,7 @@ import json
 import paho.mqtt.client as mqtt
 
 from dashboard import DashboardUI
-from components import ElevatorUI, FloorUI
+from components import ElevatorUI, FloorUI, FLOOR_COUNT
 
 
 class AsyncioHelper:
@@ -59,21 +59,17 @@ class AsyncMQTT:
         self.client.on_connect = self.on_connect
         self.client.on_message = self.on_message
 
-        self.client.message_callback_add(f"simulation/reset", self.on_simulation_reset)
-        self.client.message_callback_add(
-            "simulation/passengers/expected", self.on_expected_passengers
-        )
-        self.client.message_callback_add(
-            f"floor/+/arrived_passenger", self.on_arrived_passenger
-        )
-        self.client.message_callback_add(f"elevator/+/status", self.on_elevator_status)
-        self.client.message_callback_add(f"elevator/+/queue", self.on_queue_update)
-        self.client.message_callback_add(
-            f"elevator/+/passengerEnter", self.on_passenger_enter
-        )
-        self.client.message_callback_add(
-            f"simulation/config/passengerList/floor/+", self.on_new_passengers
-        )
+        # (topic, callback)
+        callbacks = [
+            ("floor/+/waiting_count", self.on_floor_waiting_count),
+            ("elevator/+/actual_floor", self.on_elevator_actual_floor),
+            ("elevator/+/capacity", self.on_elevator_capacity),
+            ("elevator/+/queue", self.on_elevator_queue),
+            ("simulation/floor/+/passenger_arrived", self.on_passenger_arrived),
+        ]
+
+        for c in callbacks:
+            self.client.message_callback_add(c[0], c[1])
 
         AsyncioHelper(self.loop, self.client)
 
@@ -83,63 +79,82 @@ class AsyncMQTT:
     def on_connect(self, client, userdata, flags, rc):
         client.subscribe("#")
 
-    def on_expected_passengers(self, client, userdata, msg):
-        expected = json.loads(msg.payload)
-        for f in expected.keys():
-            self.dashboard.set_passenger_count(int(f), expected=expected[f])
-
-    def on_arrived_passenger(self, client, userdata, msg):
-        floor_level = int(msg.topic.split("/")[1])
-        payload = json.loads(msg.payload)
-
-        # order is important here. Average time needs the updated passenger count
-        self.dashboard.set_passenger_count(floor_level, arrived=payload["count"])
-        self.dashboard.set_wait_time(floor_level, payload["wait_time"])
-
-    def on_elevator_status(self, client, userdata, msg):
-        id = int(msg.topic.split("/")[1])
-        payload: dict = json.loads(msg.payload)
-
-        elevator: ElevatorUI = self.dashboard.get_elevator(id)
-        state = str(payload["state"])
-        capacity = int(payload["current_capacity"])
-        position = int(payload["current_position"])
-
-        elevator.set_statebox_text(state=state, capacity=capacity)
-        elevator.set_position(position)
-
-    def on_queue_update(self, client, userdata, msg):
-        id = int(msg.topic.split("/")[1])
-        queue_list: str = str([int(dst) for dst in json.loads(msg.payload)])
-
-        self.dashboard.set_queue(id, queue_list)
-
-    def on_new_passengers(self, client, userdata, msg):
-        level = int(msg.topic.split("/")[4])
-        passenger_list: dict = json.loads(msg.payload)
-
-        floor: FloorUI = self.dashboard.get_floor(level)
-        floor.set_waiting_count(floor.get_waiting_count() + len(passenger_list))
-
-    def on_passenger_enter(self, client, userdata, msg):
-        payload: dict = json.loads(msg.payload)
-        level = payload["floor"]
-        enter_count = len(payload["enter_list"])
-        if enter_count <= 0:
+    def on_floor_waiting_count(self, client, userdata, msg):
+        floor = int(msg.topic.split("/")[1])
+        if floor >= FLOOR_COUNT:
+            # ignore error and exit
             return
 
-        floor: FloorUI = self.dashboard.get_floor(level)
-        floor.set_waiting_count(floor.get_waiting_count() - enter_count)
+        count = json.loads(msg.payload)
+        assert isinstance(count, int)
 
-        id = int(msg.topic.split("/")[1])
-        elevator: ElevatorUI = self.dashboard.get_elevator(id)
-        capacity = elevator.get_capacity() + enter_count
-        elevator.set_statebox_text(state="ENTER", capacity=capacity)
+        floor: FloorUI = self.dashboard.get_floor(floor)
+        floor.set_waiting_count(count)
 
-    def on_simulation_reset(self, client, userdata, msg):
-        # message are ignored
-        self.dashboard.reset()
+    def on_elevator_actual_floor(self, client, userdata, msg):
+        pass
+
+    def on_elevator_capacity(self, client, userdata, msg):
+        pass
+
+    def on_elevator_queue(self, client, userdata, msg):
+        pass
+
+    def on_passenger_arrived(self, client, userdata, msg):
+        pass
+
+    # def on_expected_passengers(self, client, userdata, msg):
+    #     expected = json.loads(msg.payload)
+    #     for f in expected.keys():
+    #         self.dashboard.set_passenger_count(int(f), expected=expected[f])
+
+    # def on_arrived_passenger(self, client, userdata, msg):
+    #     floor_level = int(msg.topic.split("/")[1])
+    #     payload = json.loads(msg.payload)
+
+    #     # order is important here. Average time needs the updated passenger count
+    #     self.dashboard.set_passenger_count(floor_level, arrived=payload["count"])
+    #     self.dashboard.set_wait_time(floor_level, payload["wait_time"])
+
+    # def on_elevator_status(self, client, userdata, msg):
+    #     id = int(msg.topic.split("/")[1])
+    #     payload: dict = json.loads(msg.payload)
+
+    #     elevator: ElevatorUI = self.dashboard.get_elevator(id)
+    #     state = str(payload["state"])
+    #     capacity = int(payload["current_capacity"])
+    #     position = int(payload["current_position"])
+
+    #     elevator.set_statebox_text(state=state, capacity=capacity)
+    #     elevator.set_position(position)
+
+    # def on_queue_update(self, client, userdata, msg):
+    #     id = int(msg.topic.split("/")[1])
+    #     queue_list: str = str([int(dst) for dst in json.loads(msg.payload)])
+
+    #     self.dashboard.set_queue(id, queue_list)
+
+    # def on_new_passengers(self, client, userdata, msg):
+    #     level = int(msg.topic.split("/")[4])
+    #     passenger_list: dict = json.loads(msg.payload)
+
+    #     floor: FloorUI = self.dashboard.get_floor(level)
+    #     floor.set_waiting_count(floor.get_waiting_count() + len(passenger_list))
+
+    # def on_passenger_enter(self, client, userdata, msg):
+    #     payload: dict = json.loads(msg.payload)
+    #     level = payload["floor"]
+    #     enter_count = len(payload["enter_list"])
+    #     if enter_count <= 0:
+    #         return
+
+    #     floor: FloorUI = self.dashboard.get_floor(level)
+    #     floor.set_waiting_count(floor.get_waiting_count() - enter_count)
+
+    #     id = int(msg.topic.split("/")[1])
+    #     elevator: ElevatorUI = self.dashboard.get_elevator(id)
+    #     capacity = elevator.get_capacity() + enter_count
+    #     elevator.set_statebox_text(state="ENTER", capacity=capacity)
 
     def on_message(self, client, userdata, msg):
-        # print("HHLLOO")
         pass
