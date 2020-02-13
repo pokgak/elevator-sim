661140aee5fe838ffa91297d22f72bf49c8aad3d
M Aiman Ismail <muhammadaimanbin.ismail@haw-hamburg.de>
2020-02-13 18:58:25 +0100

    gui: cleanup old async code

--- gui/async_mqtt.py
+++ gui/async_mqtt.py
@@ -91,58 +91,5 @@ class MQTTclient:
         # TODO
         pass
 
-    # def on_expected_passengers(self, client, userdata, msg):
-    #     expected = json.loads(msg.payload)
-    #     for f in expected.keys():
-    #         self.dashboard.set_passenger_count(int(f), expected=expected[f])
-
-    # def on_arrived_passenger(self, client, userdata, msg):
-    #     floor_level = int(msg.topic.split("/")[1])
-    #     payload = json.loads(msg.payload)
-
-    #     # order is important here. Average time needs the updated passenger count
-    #     self.dashboard.set_passenger_count(floor_level, arrived=payload["count"])
-    #     self.dashboard.set_wait_time(floor_level, payload["wait_time"])
-
-    # def on_elevator_status(self, client, userdata, msg):
-    #     id = int(msg.topic.split("/")[1])
-    #     payload: dict = json.loads(msg.payload)
-
-    #     elevator: ElevatorUI = self.dashboard.get_elevator(id)
-    #     state = str(payload["state"])
-    #     capacity = int(payload["current_capacity"])
-    #     position = int(payload["current_position"])
-
-    #     elevator.set_statebox_text(state=state, capacity=capacity)
-    #     elevator.set_floor(position)
-
-    # def on_queue_update(self, client, userdata, msg):
-    #     id = int(msg.topic.split("/")[1])
-    #     queue_list: str = str([int(dst) for dst in json.loads(msg.payload)])
-
-    #     self.dashboard.set_queue(id, queue_list)
-
-    # def on_new_passengers(self, client, userdata, msg):
-    #     level = int(msg.topic.split("/")[4])
-    #     passenger_list: dict = json.loads(msg.payload)
-
-    #     floor: FloorUI = self.dashboard.get_floor(level)
-    #     floor.set_waiting_count(floor.get_waiting_count() + len(passenger_list))
-
-    # def on_passenger_enter(self, client, userdata, msg):
-    #     payload: dict = json.loads(msg.payload)
-    #     level = payload["floor"]
-    #     enter_count = len(payload["enter_list"])
-    #     if enter_count <= 0:
-    #         return
-
-    #     floor: FloorUI = self.dashboard.get_floor(level)
-    #     floor.set_waiting_count(floor.get_waiting_count() - enter_count)
-
-    #     id = int(msg.topic.split("/")[1])
-    #     elevator: ElevatorUI = self.dashboard.get_elevator(id)
-    #     capacity = elevator.get_capacity() + enter_count
-    #     elevator.set_statebox_text(state="ENTER", capacity=capacity)
-
     def on_message(self, client, userdata, msg):
         pass
